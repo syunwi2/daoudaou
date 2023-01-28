939de2fd48820da6a021e8b33c2b27f10329b081
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2023-01-28 14:35:15 +0900

    템플릿 업로드

--- daoudaou.py
+++ daoudaou.py
@@ -6,27 +6,61 @@ app = Flask(__name__)
 """
     로그인 페이지
 """
-@app.route('/')
+
+
+@app.route("/")
 def login():
-   return render_template('login.html', isLoginned=None)
+    return render_template("login.html", isLoginned=None)
+
 
 """
     로그인 성공 : 일정 페이지로 이동
     로그인 실패 : 로그인 페이지로 이동
-""" 
-@app.route('/login', methods = ['POST', 'GET'])
+"""
+
+
+@app.route("/login", methods=["POST", "GET"])
 def result():
-   if request.method == 'POST':
-    id = request.form['id']
-    pw = request.form['pw']
-      
-    isLoginned = User.Login.test_login(id, pw)
-      
-    if isLoginned:
-        return render_template('user.html', isLoginned=isLoginned)
-    
-    else:
-        return render_template('login.html', isLoginned=isLoginned)
- 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port = 80, debug = True)
+    if request.method == "POST":
+        id = request.form["id"]
+        pw = request.form["pw"]
+
+        isLoginned = User.Login.test_login(id, pw)
+
+        if isLoginned:
+            return render_template("user.html", isLoginned=isLoginned)
+
+        else:
+            return render_template("login.html", isLoginned=isLoginned)
+
+
+# 일회성 일정
+@app.route("/event")
+def event():
+    page = True
+    my_list = [
+        {
+            "datetime": "10월 7일 10시 30분",
+            "title": "뭐 할까요?",
+            "content": "앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥",
+        },
+    ]
+    return render_template("event.html", events=my_list)
+
+
+# 반복 일정
+@app.route("/routine")
+def routine():
+    page = True
+    my_list = [
+        {
+            "datetime": "10월 7일 10시 30분",
+            "title": "뭐 할까요?",
+            "content": "앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥앙냥냥",
+        },
+    ]
+    return render_template("routine.html", events=my_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
