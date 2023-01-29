bb975b9e57ca7ae9001e3bac897ec9546d56c9df
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2023-01-29 16:57:54 +0900

    이벤트 삭제 완료

--- pkg/User.py
+++ pkg/User.py
@@ -1,6 +1,6 @@
 import pkg.Conn as Conn
 import pkg.make_irreg as irreg
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 """
     firstpage
@@ -8,25 +8,31 @@ from flask import render_template
     parameter : None
     return : (html template)
 """
+
+
 def firstpage():
     return render_template("login.html", isLoginned=None)
-  
+
+
 """
     login
     show login page or event page, depending on the result of login
     parameter : id, pw
     return : (html template)
 """
+
+
 def login(id, pw):
     isLoginned = test_login(id, pw)
 
-    # Problem : 데이터베이스에서 이
     if isLoginned:
-        events = irreg.irreg_view()
-        return render_template("event.html", events=events)
+        # events = irreg.irreg_view()
+        # return render_template("event.html", events=events)
+        return redirect(url_for("event"))
 
     else:
         return render_template("login.html", isLoginned=isLoginned)
+
 
 """
     test_login
@@ -34,9 +40,11 @@ def login(id, pw):
     parameter : id, pw
     return : True/False
 """
+
+
 def test_login(id, pw):
     conn, cur = Conn.open()
-    
+
     IDquery = f'SELECT EMAIL FROM USER WHERE EMAIL = "{id}";'
     numID = cur.execute(IDquery)
 
@@ -46,7 +54,7 @@ def test_login(id, pw):
 
     PWquery = f'SELECT EMAIL FROM USER WHERE EMAIL = "{id}" AND PASSWORD = "{pw}";'
     numRightPW = cur.execute(PWquery)
-    
+
     if numRightPW is 0:
         conn.close()
         return False
@@ -54,13 +62,17 @@ def test_login(id, pw):
     conn.close()
     return True
 
+
 """
     joinPage
     show join page
     return : (html template)
 """
+
+
 def joinPage():
     return render_template("join.html")
+
 
 """
     join
@@ -68,13 +80,17 @@ def joinPage():
     parameter : id, name, pw
     return : (html template)
 """
+
+
 def join(id, name, pw, pw2):
 
     # --- CHECK THE FORM ---
     # if there is blank in form, show notification.
     noID, noName, noPW, noPW2 = isBlankInForm(id, name, pw, pw2)
     if noID or noName or noPW or noPW2:
-        return render_template("join.html", noID=noID, noName=noName, noPW=noPW, noPW2=noPW2)
+        return render_template(
+            "join.html", noID=noID, noName=noName, noPW=noPW, noPW2=noPW2
+        )
 
     # if there is no blank in form, check the email is valid.
     unvalidID = isIDunvalid(id)
@@ -87,7 +103,7 @@ def join(id, name, pw, pw2):
         return render_template("join.html", isEmailExist=True)
 
     # if pw and pw2 is different, show notification.
-    isPWsame = (pw == pw2)
+    isPWsame = pw == pw2
     if isPWsame is False:
         return render_template("join.html", isPWsame=False)
 
@@ -99,25 +115,29 @@ def join(id, name, pw, pw2):
         else:
             return render_template("join.html")
 
+
 """
     isBlankInForm
     get data from user and check is it completed.
     parameter : id, name, pw, pw2
     return : tuple of (True/False)
 """
+
+
 def isBlankInForm(id, name, pw, pw2):
     noID, noName, noPW, noPW2 = False, False, False, False
 
     if id is "":
-        noID=True
+        noID = True
     if name is "":
-        noName=True
+        noName = True
     if pw is "":
-        noPW=True
+        noPW = True
     if (noPW is False) and (pw2 is ""):
-        noPW2=True
+        noPW2 = True
 
     return noID, noName, noPW, noPW2
+
 
 """
     isIDunvalid
@@ -125,16 +145,19 @@ def isBlankInForm(id, name, pw, pw2):
     parameter : id
     return : (True/False)    
 """
+
+
 def isIDunvalid(id):
-    numAt = id.count('@')
+    numAt = id.count("@")
     if numAt != 1:
         return True
     else:
-        numDot = id.split('@')[1].count('.')
+        numDot = id.split("@")[1].count(".")
         if numDot < 1:
             return True
         else:
-            return False 
+            return False
+
 
 """
     isIDused
@@ -142,6 +165,8 @@ def isIDunvalid(id):
     parameter : id
     return : (True/False)    
 """
+
+
 def isIDused(id):
     isIDused = True
 
@@ -157,17 +182,20 @@ def isIDused(id):
         conn.close()
         return isIDused
 
+
 """
     insertData
     insert id, name, pw value into DB
     return : True
 """
+
+
 def insertData(id, name, pw):
-        conn, cur = Conn.open()
+    conn, cur = Conn.open()
 
-        insertQuery = f'INSERT INTO USER VALUES("{id}", "{name}", "{pw}");'
-        cur.execute(insertQuery)
-        conn.commit()
+    insertQuery = f'INSERT INTO USER VALUES("{id}", "{name}", "{pw}");'
+    cur.execute(insertQuery)
+    conn.commit()
 
-        conn.close()
-        return True
+    conn.close()
+    return True
--- pkg/make_reg.py
+++ pkg/make_reg.py
@@ -4,31 +4,31 @@ from flask import request
 import pymysql
 
 # 1. 비정기 스케쥴 등록하기
-#서버에서 받아오기
+# 서버에서 받아오기
+
 
 def get_info_reg():
-    days = ['mon', 'tue', 'wed','thu','fri','sat','sun']
+    days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
     decimal = 0
     i = 64
     for day in days:
         if day in request.form.keys():
             decimal += i
-            i = int(i/2)
+            i = int(i / 2)
         else:
-            i = int(i/2)
+            i = int(i / 2)
             continue
-    
+
     sc_day = decimal
-    sc_email = '22' #사용자 이메일
-#    sc_day = request.form['event_day']
-    sc_title = request.form['event_title'] #일정 제목
-    sc_content = request.form['event_content'] #일정 내용
-    sc_time = request.form['event_time']
+    sc_email = "22"  # 사용자 이메일
+    #    sc_day = request.form['event_day']
+    sc_title = request.form["event_title"]  # 일정 제목
+    sc_content = request.form["event_content"]  # 일정 내용
+    sc_time = request.form["event_time"]
     # request.form key 명칭...
     return (sc_email, sc_day, sc_title, sc_content, sc_time)
 
 
- 
 # DB 추가
 def make_reg():
     email = get_info_reg()[0]
@@ -36,31 +36,34 @@ def make_reg():
     title = get_info_reg()[2]
     content = get_info_reg()[3]
     time = str(get_info_reg()[4])
-    
-    conn = pymysql.connect(host = 'localhost',
-                       user = 'root',
-                       password = 'root1234',
-                       db = 'daoudaou',
-                       charset = 'utf8')
+
+    conn = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root1234",
+        db="daoudaou",
+        charset="utf8",
+    )
     cur = conn.cursor()
-    
-    
+
     sql_reg = f'insert into routine(email,day,title,content,time) values("{email}","{day}","{title}","{content}","{time}")'
     cur.execute(sql_reg)
     conn.commit()
     conn.close()
-    
+
+
 # 2. 일정 조회
 def reg_view():
-    conn = pymysql.connect(host = 'localhost',
-                       user = 'root',
-                       password = 'root1234',
-                       db = 'daoudaou',
-                       charset = 'utf8')
+    conn = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root1234",
+        db="daoudaou",
+        charset="utf8",
+    )
     cur = conn.cursor()
 
-    
-    sql_view2 = 'select * from routine order by day desc' # 정렬 기능.. ??? 일단 앞 요일일수록 숫자 크니까 역순으로 정렬...
+    sql_view2 = "select * from routine order by day desc"  # 정렬 기능.. ??? 일단 앞 요일일수록 숫자 크니까 역순으로 정렬...
     cur.execute(sql_view2)
 
     schedule_list_reg = []
@@ -72,29 +75,30 @@ def reg_view():
         scd[5] = str(scd[5])
     for scd in schedule_list_reg:
         dict = {}
-        dict['routine_key'] = scd[0]
-        dict['routine_title'] = scd[3]
-        dict['routine_day'] = scd[2]
-        dict['routine_time'] = scd[5]
-        dict['routine_content'] = scd[4]
+        dict["routine_key"] = scd[0]
+        dict["routine_title"] = scd[3]
+        dict["routine_day"] = scd[2]
+        dict["routine_time"] = scd[5]
+        dict["routine_content"] = scd[4]
         reg_dict_list.append(dict)
-    
+
     conn.commit()
     conn.close()
     return reg_dict_list
 
+
 # 3. 일정 삭제
-def erase_routine():
-    key = request.form['routine_key']
-
-    conn = pymysql.connect(host = 'localhost',
-                           user = 'root',
-                           password = 'root1234',
-                           db = 'daoudaou',
-                           charset = 'utf8')
+def erase_routine(key, sort_of):
+    conn = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root1234",
+        db="daoudaou",
+        charset="utf8",
+    )
     cur = conn.cursor()
-
-    sql_erase1 = f'delete from routine where event_key = "{key}"'
+    # sql_erase1 = f'delete from routine where event_key = "{key}"'
+    sql_erase1 = f'delete from {sort_of} where {sort_of}_key = "{key}"'
     cur.execute(sql_erase1)
 
     conn.commit()
