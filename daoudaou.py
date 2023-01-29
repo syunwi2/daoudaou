6c2be9a4524dad4a0129b14a91a3ce80d0d76b1b
joooohyung <123712578+joooohyung@users.noreply.github.com>
2023-01-30 08:56:19 +0900

    Update daoudaou.py

--- daoudaou.py
+++ daoudaou.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import pkg.User as User
 from pkg import make_irreg
 from pkg import make_reg
 from pkg.mail import sched_del_event, sched_send
-from pkg import User
 
 app = Flask(__name__)
-app.secret_key = "Key"
 
 
 """
@@ -25,12 +23,9 @@ def firstpage():
     else:
         return User.firstpage()
 
-
 """
     회원 가입 
 """
-
-
 @app.route("/join", methods=["POST", "GET"])
 def join():
     if request.method == "POST":
@@ -42,17 +37,6 @@ def join():
     else:
         return User.joinPage()
 
-
-"""
-    로그아웃
-"""
-
-
-@app.route("/logout")
-def logout():
-    return User.logout()
-
-
 """
     일회성 일정 페이지
 """
@@ -60,10 +44,8 @@ def logout():
 
 @app.route("/event")
 def event():
-    user_name = make_irreg.get_name()
     my_list = make_irreg.irreg_view()
-    print(user_name)
-    return render_template("event.html", events=my_list, name=user_name)
+    return render_template("event.html", events=my_list)
 
 
 """
@@ -74,58 +56,28 @@ def event():
 @app.route("/routine")
 def routine():
     page = True
-    user_name = make_irreg.get_name()
     my_list = make_reg.reg_view()
-    day_list = ["월", "화", "수", "목", "금", "토", "일"]
-    for routine in my_list:
-        my_day_list = []
-        day_number = routine["routine_day"]
-        day_number = bin(day_number)[2:]
-        day_number = "0" * (7 - len(day_number)) + day_number
-        for i in range(0, 7):
-            if day_number[i] == "1":
-                my_day_list.append(day_list[i])
-        routine["routine_day"] = ", ".join(my_day_list)
-    return render_template("routine.html", events=my_list, name=user_name)
+    return render_template("routine.html", events=my_list)
 
 
 @app.route("/send_event", methods=["POST"])
 def send_event():
     make_irreg.get_info_irreg()
     make_irreg.make_irreg()
-    my_list = []
-    return redirect(url_for("event"))
-
+    my_list=[]
+    return render_template("event.html", events=my_list)
 
 sched_del_event
 sched_send
-
 
 @app.route("/send_routine", methods=["POST"])
 def send_routine():
     make_reg.get_info_reg()
     make_reg.make_reg()
-    my_list = []
-    return redirect(url_for("routine"))
-
-
-# 이벤트 삭제
-@app.route("/erase_event", methods=["delete"])
-def erase_event():
-    key = request.get_json()["key"]
-    sort_of = "event"
-    make_reg.erase_routine(key, sort_of)
-    return "success"
-
-
-# 루틴 삭제
-@app.route("/erase_routine", methods=["delete"])
-def erase_routine():
-    key = request.get_json()["key"]
-    sort_of = "routine"
-    make_reg.erase_routine(key, sort_of)
-    return "success"
+    my_list =[]
+    return render_template("routine.html", events=my_list)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
+
