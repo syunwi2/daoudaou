9b1163109c366d392fd74ab79c84100c1cc39d98
syunwi2 <syunwi2@gmail.com>
2023-01-29 17:37:54 +0900

    1. routinr.html, evet.html /logout 으로 연결 url 수정 2. User.py의 logout 함수 파라미터 수정

--- pkg/User.py
+++ pkg/User.py
@@ -8,10 +8,10 @@ from flask import render_template, session, redirect, url_for
     parameter : None
     return : (html template)
 """
-def logout(id, pw):
+def logout():
     session.pop('id', None)
     return redirect(url_for('firstpage', isLoginned=None))
-    
+
 
 """
     firstpage
@@ -20,14 +20,11 @@ def logout(id, pw):
     parameter : None
     return : (html template)
 """
-
-
 def firstpage():
     if 'id' in session:
         return redirect(url_for('event'))
     else:
         return render_template("login.html", isLoginned=None)
-  
   
 """
     login
@@ -35,8 +32,6 @@ def firstpage():
     parameter : id, pw
     return : (html template)
 """
-
-
 def login(id, pw):
     isLoginned = test_login(id, pw)
 
@@ -47,18 +42,15 @@ def login(id, pw):
     else:
         return render_template("login.html", isLoginned=isLoginned)
 
-
 """
     test_login
     check the input ID and PASSWORD is in the DB and right pair.
     parameter : id, pw
     return : True/False
 """
-
-
 def test_login(id, pw):
     conn, cur = Conn.open()
-
+    
     IDquery = f'SELECT EMAIL FROM USER WHERE EMAIL = "{id}";'
     numID = cur.execute(IDquery)
 
@@ -68,7 +60,7 @@ def test_login(id, pw):
 
     PWquery = f'SELECT EMAIL FROM USER WHERE EMAIL = "{id}" AND PASSWORD = "{pw}";'
     numRightPW = cur.execute(PWquery)
-
+    
     if numRightPW is 0:
         conn.close()
         return False
@@ -76,17 +68,13 @@ def test_login(id, pw):
     conn.close()
     return True
 
-
 """
     joinPage
     show join page
     return : (html template)
 """
-
-
 def joinPage():
     return render_template("join.html")
-
 
 """
     join
@@ -94,17 +82,13 @@ def joinPage():
     parameter : id, name, pw
     return : (html template)
 """
-
-
 def join(id, name, pw, pw2):
 
     # --- CHECK THE FORM ---
     # if there is blank in form, show notification.
     noID, noName, noPW, noPW2 = isBlankInForm(id, name, pw, pw2)
     if noID or noName or noPW or noPW2:
-        return render_template(
-            "join.html", noID=noID, noName=noName, noPW=noPW, noPW2=noPW2
-        )
+        return render_template("join.html", noID=noID, noName=noName, noPW=noPW, noPW2=noPW2)
 
     # if there is no blank in form, check the email is valid.
     unvalidID = isIDunvalid(id)
@@ -117,7 +101,7 @@ def join(id, name, pw, pw2):
         return render_template("join.html", isEmailExist=True)
 
     # if pw and pw2 is different, show notification.
-    isPWsame = pw == pw2
+    isPWsame = (pw == pw2)
     if isPWsame is False:
         return render_template("join.html", isPWsame=False)
 
@@ -129,29 +113,25 @@ def join(id, name, pw, pw2):
         else:
             return render_template("join.html")
 
-
 """
     isBlankInForm
     get data from user and check is it completed.
     parameter : id, name, pw, pw2
     return : tuple of (True/False)
 """
-
-
 def isBlankInForm(id, name, pw, pw2):
     noID, noName, noPW, noPW2 = False, False, False, False
 
     if id is "":
-        noID = True
+        noID=True
     if name is "":
-        noName = True
+        noName=True
     if pw is "":
-        noPW = True
+        noPW=True
     if (noPW is False) and (pw2 is ""):
-        noPW2 = True
+        noPW2=True
 
     return noID, noName, noPW, noPW2
-
 
 """
     isIDunvalid
@@ -159,19 +139,16 @@ def isBlankInForm(id, name, pw, pw2):
     parameter : id
     return : (True/False)    
 """
-
-
 def isIDunvalid(id):
-    numAt = id.count("@")
+    numAt = id.count('@')
     if numAt != 1:
         return True
     else:
-        numDot = id.split("@")[1].count(".")
+        numDot = id.split('@')[1].count('.')
         if numDot < 1:
             return True
         else:
-            return False
-
+            return False 
 
 """
     isIDused
@@ -179,8 +156,6 @@ def isIDunvalid(id):
     parameter : id
     return : (True/False)    
 """
-
-
 def isIDused(id):
     isIDused = True
 
@@ -196,20 +171,17 @@ def isIDused(id):
         conn.close()
         return isIDused
 
-
 """
     insertData
     insert id, name, pw value into DB
     return : True
 """
-
-
 def insertData(id, name, pw):
-    conn, cur = Conn.open()
+        conn, cur = Conn.open()
 
-    insertQuery = f'INSERT INTO USER VALUES("{id}", "{name}", "{pw}");'
-    cur.execute(insertQuery)
-    conn.commit()
+        insertQuery = f'INSERT INTO USER VALUES("{id}", "{name}", "{pw}");'
+        cur.execute(insertQuery)
+        conn.commit()
 
-    conn.close()
-    return True
+        conn.close()
+        return True
